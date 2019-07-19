dc4b90c70f43a18b6f07dc131b4a8790c61b3a66
FRAMGIA\nguyen.van.minhb <minhnv2306>
2019-07-19 14:28:30 +0700

    delete task

--- client/src/helpers/task.js
+++ client/src/helpers/task.js
@@ -1,3 +1,5 @@
+import { config as configTask } from './../config/task';
+
 export function isAssignedToMe(task, userId) {
   let assignedToMe = false;
   const assignees = task.assignees;
@@ -10,4 +12,21 @@ export function isAssignedToMe(task, userId) {
   }
 
   return assignedToMe;
+}
+
+export function isDoneTask(task, userId) {
+  let isDone = false;
+  const assignees = task.assignees;
+
+  for (let i = 0; i < assignees.length; i++) {
+    if (
+      assignees[i].user == userId &&
+      (assignees[i].percent == 100 || assignees[i].status == configTask.STATUS.DONE.VALUE)
+    ) {
+      isDone = true;
+      break;
+    }
+  }
+
+  return isDone;
 }
